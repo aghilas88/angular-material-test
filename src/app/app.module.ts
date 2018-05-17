07a152e36f5df7aa07a219494c8799f2278a7f4f
aghilas88 <aghilasdjm@gmail.com>
2018-05-17 11:54:41 +0200

    ADD DIFFERENTS COMPONENTS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MaterialModule } from './/material.module';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductResumeComponent } from './product-resume/product-resume.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    SidenavComponent,
+    ProductListComponent,
+    ProductDetailsComponent,
+    ProductResumeComponent,
+    SearchBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [MediaMatcher],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
